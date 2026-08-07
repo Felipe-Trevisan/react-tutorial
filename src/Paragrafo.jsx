07d395569fcc07ecc512ae3e856d5be5b1bd8dee
Felipe Trevisan <felipetrevisan360@gmail.com>
2026-08-07 07:57:19 -0300

    Initial commit

---
+++ src/Paragrafo.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+const Paragrafo = (props) => {
+    const [pontos, setPontos] = useState(0);
+
+    const aumentarPontos = () => {
+        setPontos(pontos + 1)
+    }
+
+    const diminuirPontos = () => {
+        setPontos(pontos - 1)
+    }
+
+    return (
+        <div>
+            <p>{props.nome} tem {pontos} pontos.
+            <button onClick={aumentarPontos}>+</button>
+            <button onClick={diminuirPontos}>-</button>
+            </p>
+            
+        </div>
+    );
+}
+
+export default Paragrafo
